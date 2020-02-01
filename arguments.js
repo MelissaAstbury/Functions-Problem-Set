bc7c6438d562965755d6294a1cc74f8c4807a684
Melissa Astbury <melissajastbury@gmail.com>
2020-02-01 18:40:10 +0000

    Functions, setIntervals & ES6

--- arguments.js
+++ arguments.js
@@ -2,17 +2,17 @@
 //return if even
 //return false otherwise
 
-// function isEven(num) {
-//   if (num % 2 === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// function isEven(num) {
-//   return num % 2 === 0;
-// }
+function isEven(num) {
+  return num % 2 === 0;
+}
 
 //Using Factorial
 
@@ -26,3 +26,54 @@ function Factorial(num) {
   //return the result variable
   return result;
 }
+
+//kebabToSnake
+//replace all '-' with "_"
+//return str
+
+function kebabToSnake(str) {
+  return str.replace(/-/g, "_");
+}
+
+// console.log(kebabToSnake("hello-to-goodbye"));
+
+// Function which expects a number and a string
+// Returns true if number is > 9 and string length =< 3
+
+function numberWithString(str, num) {
+  let andy = "wandy";
+  console.log(andy);
+  if (num > 9 && str.length <= 3) {
+    return true;
+  } else if (num === 2) {
+    return "mangos";
+  } else {
+    return false;
+  }
+}
+
+//console.log(numberWithString("three", 3));
+
+function singSong() {
+  console.log("blah blah black sheep");
+  console.log("have you any wool");
+}
+
+singSongTwo = () => {
+  console.log("blah blah black sheep");
+  console.log("have you any wool");
+};
+
+singSongTwo();
+
+//setInterval(singSong, 1500);
+
+// setInterval(function() {
+//   console.log("blah blah black sheep");
+//   console.log("have you any wool");
+// }, 2000);
+
+// setInterval(() => {
+//   console.log("blah blah black sheep");
+//   console.log("have you any wool");
+// });
